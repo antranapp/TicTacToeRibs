7b713f95f185b843675c594c897e5c7371a1c6d9
An Tran <tran.binhan@gmail.com>
2019-07-02 17:34:20 +0200

    initial commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform :ios, '10.0'
+
+use_frameworks!
+inhibit_all_warnings!
+
+target 'TicTacToeRibs' do
+  pod 'RIBs', :git => 'https://github.com/uber/RIBs'
+  pod 'SnapKit', '~> 5.0.0'
+  pod 'RxCocoa', '~> 4.5.0'
+end
---
+++ TicTacToeRibs/Promo/Stub.swift
@@ -0,0 +1,13 @@
+//
+//  Stub.swift
+//  TicTacToeRibs
+//
+//  Created by An Tran on 02.07.19.
+//  Copyright © 2019 An Tran. All rights reserved.
+//
+
+import RIBs
+
+public class LaunchGameWorkflow: Workflow<RootActionableItem> {
+    init(url: URL) { super.init() }
+}
